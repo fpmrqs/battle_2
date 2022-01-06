e1941733515353900b071d5c87accc3167b843d6
Neto <fpottinger.marques@gmail.com>
2022-01-06 15:50:31 +0000

    Used sessions to redirect '/names' post query to get

--- app.rb
+++ app.rb
@@ -1,18 +1,27 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+
 class Battle < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end 
-
+  
 get '/' do
   erb(:index)
 end
 
-post '/names' do
-  @player_1 = params[:player_1]
+get '/play' do
+  @player_1 = session['player_1']
+  @player_2 = session['player_2']
   erb(:play)
+end
+
+post '/names' do
+  session['player_1'] = params[:player_1]
+  session['player_2'] = params[:player_2]
+  redirect to('/play')
 end
 
 run! if app_file == $0
